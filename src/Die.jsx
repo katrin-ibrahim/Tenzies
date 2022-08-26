9c597f6579407497305e86e150c56a9e031f1681
katrin-ibrahim <katrinemadfathy@gmail.com>
2022-08-26 23:57:49 +0200

    added dice roling and holding

--- src/Die.jsx
+++ src/Die.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Die(props) {
   return (
-    <div className='die'><h2>{props.value}</h2></div>
+    <div className={props.isHeld? 'die held': 'die not-held' } onClick={props.holdDice}>
+        <h2>{props.value}</h2>
+    </div>
   )
 }
