2eb0a0f66444e510698824254994a8593c9875b6
katrin-ibrahim <katrinemadfathy@gmail.com>
2022-08-27 23:37:55 +0200

    added game ending

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Die from './Die'
 import {nanoid} from "nanoid"
-
+import Confetti from "react-confetti"
 
 function App() {
 
   const [dice,setDice] = useState(allNewDice()) 
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() =>{
+    console.log(tenzies)
+    const heldDice = dice.filter(die => die.isHeld)
+    const value = dice[0].value
+    const sameValDice = heldDice.filter(die => (die.value === value))
+    if(sameValDice.length === 10){
+      setTenzies(true)
+      console.log("You won!")
+    }
+
+  },[dice])
 
   function generateNewDie(){
       return { 
@@ -24,17 +37,23 @@ function App() {
     return newDice
   
   }
-
   const diceElements = dice.map(die => (
   <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} /> 
   ))
   
   function rollDice(){
-    setDice(prevDice => prevDice.map(die => {
+    if(!tenzies){
+      setDice(prevDice => prevDice.map(die => {
       return die.isHeld? 
       die:
       generateNewDie()
     }))
+  }
+    else{
+      setTenzies(false)
+      setDice(allNewDice())
+  }
+    
   } 
 
   function holdDice(id) {
@@ -50,12 +69,15 @@ function App() {
   return (
    
       <main>
+        {tenzies && <Confetti/>}
         <h1 className="title">Tenzies</h1>
         <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="die-container">
           {diceElements}
         </div>
-        <button className='roll-dice' onClick={rollDice}>Roll</button>
+        <button
+         className='roll-dice' onClick={rollDice}>{tenzies?"New Game":"Roll"}
+        </button>
       </main>
   
   )
