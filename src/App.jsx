51ecef1296e86eb9fbdcef014b3f38e03dd9b323
katrin-ibrahim <katrinemadfathy@gmail.com>
2022-08-27 22:27:57 +0200

    added game instructionsand fixed holdDice()
    holdDice() now maintins selecteddice between rolls

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,18 @@ function App() {
 
   const [dice,setDice] = useState(allNewDice()) 
 
+  function generateNewDie(){
+      return { 
+        value: Math.ceil(Math.random() * 6), 
+        isHeld:false,
+        id: nanoid()
+        }
+  }
+
   function allNewDice(){
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push({ 
-          value: Math.ceil(Math.random() * 6), 
-          isHeld:false,
-          id: nanoid()
-          })
+        newDice.push(generateNewDie())
     }
     return newDice
   
@@ -26,7 +30,11 @@ function App() {
   ))
   
   function rollDice(){
-    setDice(allNewDice())
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld? 
+      die:
+      generateNewDie()
+    }))
   } 
 
   function holdDice(id) {
@@ -42,6 +50,8 @@ function App() {
   return (
    
       <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="die-container">
           {diceElements}
         </div>
